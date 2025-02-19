d5cbf14560bc824a9afe7508431bec6be61b237f
ImJaePang <narushel@naver.com>
2025-02-19 22:47:57 +0900

    ok sorry

---
+++ src/components/Answers.jsx
@@ -0,0 +1,32 @@
+export default function Answers() {
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer) => {
+                const isSelected =
+                    userAnswers[userAnswers.length - 1] === answer;
+                let cssClass = "";
+                if (answerState === "answered" && isSelected) {
+                    cssClass = "selected";
+                }
+
+                if (
+                    (answerState === "correct" || answerState === "wrong") &&
+                    isSelected
+                ) {
+                    cssClass = answerState;
+                }
+
+                return (
+                    <li key={answer} className="answer">
+                        <button
+                            onClick={() => handleSelectAnswer(answer)}
+                            className={cssClass}
+                        >
+                            {answer}
+                        </button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
